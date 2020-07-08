f1b6789016382930b7e27370389565e67e89d4a4
Aaron <the_flicker7@hotmail.com>
2020-07-08 18:12:27 -0400

    completed exchange rate tutorial

--- ExchangeRateCalculator/script.js
+++ ExchangeRateCalculator/script.js
@@ -12,7 +12,12 @@ function calculate() {
 
   fetch(`https://v6.exchangerate-api.com/v6/${APIKEY}/latest/${currency_one}`)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      const rate = data.conversion_rates[currency_two];
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
 }
 
 // Event Listeners
@@ -21,5 +26,11 @@ currencyEl_one.addEventListener('change', calculate);
 amountEl_one.addEventListener('input', calculate);
 currencyEl_two.addEventListener('change', calculate);
 amountEl_two.addEventListener('input', calculate);
+swapEl.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate();
+});
 
 calculate();
